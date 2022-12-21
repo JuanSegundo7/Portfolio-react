d4d5506d6e1eafb589f8429a0e5872a71b9ff9d9
Juan Segundo <juansepincha@gmail.com>
2022-12-20 23:21:17 -0300

    updating detail

--- src/Components/Projects/Detail_Projects/Detail_Project.js
+++ src/Components/Projects/Detail_Projects/Detail_Project.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Detail_Project.css"
-import Colombus from "../imgs/instrumentos-react-preview.png"
+import {Link} from "react-router-dom"
 
 const DetailProject = (props) => {
     window.scrollTo(0,0)
@@ -32,6 +32,9 @@ const DetailProject = (props) => {
     return (
         <section id="Section">
             <article id="Container" className={`${props.color === "grey" ? "grey" : "green" }`}>
+                <Link to="/" id="a-back">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256S114.6 512 256 512s256-114.6 256-256zM271 135c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-87 87 87 87c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L167 273c-9.4-9.4-9.4-24.6 0-33.9L271 135z"/></svg>
+                </Link>
                 <article className="flex-article">
                     <article id="left-side-article">
                         <figure>
