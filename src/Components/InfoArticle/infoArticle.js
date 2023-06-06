3d535609f39ff59d08ae044eae4a38a55277a406
Juan Segundo <juansepincha@gmail.com>
2023-06-06 17:42:51 -0300

    update 6/6

--- src/Components/InfoArticle/infoArticle.js
+++ src/Components/InfoArticle/infoArticle.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import "./infoArticle.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from "react";
+import "./infoArticle.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const InfoArticle = (props) => {
-    let array = props.array
+  let array = props.array;
 
-    return (
-        <article id="wrapper">
-            <article id="wrapper-container">
-                <article id="wrapper-x">
-                    <FontAwesomeIcon icon="fa-xmark" onClick={() => props.setState(false)}/> 
-                </article>
-                    <h2>{props.title}</h2>
-                <article>
-                    {array.map((item, i) => {
-                        return(
-                            <article key={i} className="wrapper-p-article">
-                                <FontAwesomeIcon icon="fa-bolt"/> 
-                                <p>{item}</p>
-                            </article>
-                        )
-                    })}
-                </article>
-            </article>
+  return (
+    <article id="wrapper">
+      <article id="wrapper-container">
+        <div id="flex-wrapper-x">
+          <article id="wrapper-x">
+            <FontAwesomeIcon
+              icon="fa-xmark"
+              onClick={() => props.setState(false)}
+            />
+          </article>
+          <h2>{props.title}</h2>
+        </div>
+        <article id="wrapper-flex-inside">
+          {array.map((item, i) => {
+            return (
+              <article key={i} className="wrapper-p-article">
+                <FontAwesomeIcon icon="fa-bolt" />
+                <p>{item}</p>
+              </article>
+            );
+          })}
         </article>
-    );
-}
+      </article>
+    </article>
+  );
+};
 
 export default InfoArticle;
--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,89 +1,105 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import "./Skills.css"
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Skills.css";
 
 const Skills = () => {
-    return (
-        <section id="Skills">
-            <h1>Skills</h1>
-            <div id="flex-skills">
-            <article>
-                <h3>Front-end</h3>
-                <div id="p-container">
-                    <div>
-                        <div className="badges">
-                            <FontAwesomeIcon icon="fa-circle-check" />
-                            <p>HTML</p>
-                        </div>
-                        <div className="badges">
-                            <FontAwesomeIcon icon="fa-circle-check" />
-                            <p>JAVASCRIPT</p>                    
-                        </div>
-                        <div className="badges">
-                            <FontAwesomeIcon icon="fa-circle-check" />
-                            <p>AUTH0</p>
-                        </div>
-                    </div>
-                    <div>
-                        <div className="badges">
-                            <FontAwesomeIcon icon="fa-circle-check" />
-                            <p>CSS</p>
-                        </div>
-                        <div className="badges">
-                            <FontAwesomeIcon icon="fa-circle-check" />
-                            <p>REACT.JS</p>                    
-                        </div>
-                        <div className="badges">
-                            <FontAwesomeIcon icon="fa-circle-check" />
-                            <p>CLOUDINARY</p>
-                        </div>
-                    </div>
-                </div>
-            </article>
-            <article>
-                <h3>Back-end</h3>
-                <div id="p-container">
-                    <div>
-                        <div className="badges">
-                            <FontAwesomeIcon icon="fa-circle-check" />
-                            <p>NODE.JS</p>
-                        </div>
-                        <div className="badges">
-                            <FontAwesomeIcon icon="fa-circle-check" />
-                            <p>MONGOOSE</p>                    
-                        </div>
-                        <div className="badges">
-                            <FontAwesomeIcon icon="fa-circle-check" />
-                            <p>MONGO</p>
-                        </div>
-                        <div className="badges">
-                            <FontAwesomeIcon icon="fa-circle-check" />
-                            <p>MVC</p>
-                        </div>
-                    </div>
-                    <div>
-                        <div className="badges">
-                            <FontAwesomeIcon icon="fa-circle-check" />
-                            <p>EXPRESS</p>
-                        </div>
-                        <div className="badges">
-                            <FontAwesomeIcon icon="fa-circle-check" />
-                            <p>SEQUELIZE</p>                    
-                        </div>
-                        <div className="badges">
-                            <FontAwesomeIcon icon="fa-circle-check" />
-                            <p>MYSQL</p>
-                        </div>
-                        <div className="badges">
-                            <FontAwesomeIcon icon="fa-circle-check" />
-                            <p>POSTGRESQL</p>
-                        </div>
-                    </div>
-                </div>
-            </article>
-        </div>
-        </section>         
-    );
-}
+  return (
+    <section id="Skills">
+      <h1>Skills</h1>
+      <div id="flex-skills">
+        <article>
+          <h3>Front-end</h3>
+          <div id="p-container">
+            <div>
+              <div className="badges">
+                <FontAwesomeIcon icon="fa-circle-check" />
+                <p>HTML</p>
+              </div>
+              <div className="badges">
+                <FontAwesomeIcon icon="fa-circle-check" />
+                <p>TYPESCRIPT</p>
+              </div>
+              <div className="badges">
+                <FontAwesomeIcon icon="fa-circle-check" />
+                <p>NEXT</p>
+              </div>
+              <div className="badges">
+                <FontAwesomeIcon icon="fa-circle-check" />
+                <p>REACT-QUERY</p>
+              </div>
+              <div className="badges">
+                <FontAwesomeIcon icon="fa-circle-check" />
+                <p>TAILWIND</p>
+              </div>
+            </div>
+            <div>
+              <div className="badges">
+                <FontAwesomeIcon icon="fa-circle-check" />
+                <p>CSS</p>
+              </div>
+              <div className="badges">
+                <FontAwesomeIcon icon="fa-circle-check" />
+                <p>REACT.JS</p>
+              </div>
+              <div className="badges">
+                <FontAwesomeIcon icon="fa-circle-check" />
+                <p>AUTH0</p>
+              </div>
+              <div className="badges">
+                <FontAwesomeIcon icon="fa-circle-check" />
+                <p>REACT-NATIVE</p>
+              </div>
+              <div className="badges">
+                <FontAwesomeIcon icon="fa-circle-check" />
+                <p>MATERIAL UI</p>
+              </div>
+            </div>
+          </div>
+        </article>
+        <article>
+          <h3>Back-end</h3>
+          <div id="p-container">
+            <div>
+              <div className="badges">
+                <FontAwesomeIcon icon="fa-circle-check" />
+                <p>NODE.JS</p>
+              </div>
+              <div className="badges">
+                <FontAwesomeIcon icon="fa-circle-check" />
+                <p>MONGOOSE</p>
+              </div>
+              <div className="badges">
+                <FontAwesomeIcon icon="fa-circle-check" />
+                <p>MONGO</p>
+              </div>
+              <div className="badges">
+                <FontAwesomeIcon icon="fa-circle-check" />
+                <p>MVC</p>
+              </div>
+            </div>
+            <div>
+              <div className="badges">
+                <FontAwesomeIcon icon="fa-circle-check" />
+                <p>EXPRESS</p>
+              </div>
+              <div className="badges">
+                <FontAwesomeIcon icon="fa-circle-check" />
+                <p>SEQUELIZE</p>
+              </div>
+              <div className="badges">
+                <FontAwesomeIcon icon="fa-circle-check" />
+                <p>MYSQL</p>
+              </div>
+              <div className="badges">
+                <FontAwesomeIcon icon="fa-circle-check" />
+                <p>POSTGRESQL</p>
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+};
 
 export default Skills;
